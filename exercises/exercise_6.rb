a42938d09bd7bf23aa6bc7d63ac281a9ec688ed1
Vivian Zhang <vvynz12@gmail.com>
2022-03-08 17:34:06 +0000

    Create employees for stores1 and stores2 and add validation to Store class

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joon", last_name: "Kim", hourly_rate: 85)
+@store1.employees.create(first_name: "Seokjin", last_name: "Kim", hourly_rate: 80)
+@store2.employees.create(first_name: "Yoongi", last_name: "Min", hourly_rate: 85)
+@store2.employees.create(first_name: "Hoseok", last_name: "Jung", hourly_rate: 80)
+@store2.employees.create(first_name: "Jungkook", last_name: "Jeon", hourly_rate: 80)
