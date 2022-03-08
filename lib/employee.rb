a4dc7b0cea763efe52a1eb2bf7ebf7a70cd3d8ee
Vivian Zhang <vvynz12@gmail.com>
2022-03-08 17:45:59 +0000

    Fix bugs

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
-  validates: :first_name, presence: true
-  validates: :last_name, presence: true
-  validates: :hourly_rate, numericality: {in: 40..200}
-  validates_associated: :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {in: 40..200}
+  validates_associated :store
 end
