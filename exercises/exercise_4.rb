5682b451d9fa73e9a5a078dfdfc20984e53d466d
Vivian Zhang <vvynz12@gmail.com>
2022-03-08 16:46:07 +0000

    Create 3 more stores and use the where clause to fetch stores that only carry men's apparel

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,10 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+whistley = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each {|m| pp "#{m.name} makes an annual revenue of #{m.annual_revenue}"}
